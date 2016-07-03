15b5a8f2d5f8e728b71573cc18a951c21cd816c5
Austen Morgan <austencm@gmail.com>
2016-07-02 17:52:07 -0700

    Add youtube-autolike script

---
+++ youtube-autolike/youtube-autolike.js
@@ -0,0 +1,53 @@
+// ==UserScript==
+// @name         YouTube Autolike
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Magically like YouTube videos from your subscriptions
+// @author       Austen Morgan
+// @include      http://*.youtube.com/watch*v=*
+// @include      http://youtube.com/watch*v=*
+// @include      https://*.youtube.com/watch*v=*
+// @include      https://youtube.com/watch*v=*
+// @run-at       document-end
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Is user logged in?
+    if ( !document.body.classList.contains('yt-user-logged-in') )
+      return;
+
+    var likeButton = document.querySelector('.like-button-renderer-like-button-unclicked');
+    // Is video already liked?
+    if ( likeButton.classList.contains('hid') )
+      return;
+
+    var channel = document.querySelector('#watch7-user-header .yt-user-info > a.g-hovercard').textContent.trim(),
+        subscribedChannels = null,
+        // Watch for changes in the guide container so we know when the user's subscriptions are loaded
+        observeNode = document.getElementById('guide'),
+        observeConfig = {
+          childList: true,
+          subtree: true
+        },
+        observer = new MutationObserver(function(mutations) {
+
+          subscribedChannels = document.querySelectorAll('#guide-channels .display-name span');
+
+          if (subscribedChannels) {
+            // Compare the video's channel to the user's subscribed channels
+            for (var i = 0; i < subscribedChannels.length; i++) {
+              if (subscribedChannels[i].textContent.trim() === channel) {
+                likeButton.click();
+                break;
+              }
+            }
+          }
+
+        });
+
+    observer.observe(observeNode, observeConfig);
+
+})();
